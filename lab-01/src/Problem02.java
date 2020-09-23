e38e6a0b4bc7c2b07886a1377767ebdcc0c5a35b
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-09-23 14:13:30 +0600

    Problem02 solved

---
+++ lab-01/src/Problem02.java
@@ -0,0 +1,7 @@
+public class Problem02 {
+    public static void main(String[] args) {
+        System.out.println("*********************\n" +
+                           "*I like programming!*\n" +
+                           "*********************");
+    }
+}
