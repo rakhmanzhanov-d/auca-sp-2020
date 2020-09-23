57f0d4cd9fd7c8b029a457d2521c492cba74f495
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-09-23 14:07:40 +0600

    Problem01 solved

---
+++ lab-02/src/Problem01.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Problem01 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("1st value?");
+        int a = inp.nextInt();
+
+        System.out.print("2nd value?");
+        int b = inp.nextInt();
+
+        int t = a;
+        a = b;
+        b = t;
+
+        System.out.printf("Before swaping: a = %d; b = %d%n", b,a);
+        System.out.printf("After swaping: a = %d; b = %d%n", a,b);
+    }
+}
