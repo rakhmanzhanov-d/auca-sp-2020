f605820fd0c133d00e9dc4bc18e6152c5f210096
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-09-24 12:53:57 +0600

    Problem06 solved

---
+++ lab-01/src/Problem06.java
@@ -0,0 +1,8 @@
+public class Problem06 {
+    public static void main(String[] args) {
+        System.out.println("   J    A   v     v  A ");
+        System.out.println("   J   A A   v   v  A A");
+        System.out.println("J  J  AAAAA   V V  AAAAA");
+        System.out.println(" JJ  A     A   V  A     A");
+    }
+}
