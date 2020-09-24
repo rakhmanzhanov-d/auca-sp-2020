c51b1a89e589153d7b583168351e50a65c5895fb
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-09-24 13:09:16 +0600

    Problem09 solved

---
+++ lab-01/src/Problem09.java
@@ -0,0 +1,8 @@
+public class Problem09 {
+    public static void main(String[] args) {
+        double pi = 4 - (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11);
+        double Pi = 4 - (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13);
+
+        System.out.println(pi + "     " + Pi);
+    }
+}
