eb57b7a51a9e0fa79688c97c56b25e4f2d236735
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-10-27 16:53:28 +0600

    Problem-05-Solved

---
+++ lab-03/src/Problem05.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        // GRADES
+        String grade = "NP";
+        System.out.print("Number of points?");
+        int numGrade = inp.nextInt();
+        if(numGrade >= 90 && numGrade <= 100){
+            grade = "A";
+        }else if(numGrade >= 80 && numGrade <= 90){
+            grade = "B";
+        }else if (numGrade >= 70 && numGrade <= 80){
+            grade = "C";
+        }else if (numGrade >= 60 && numGrade <= 70){
+            grade = "D";
+        }else if (numGrade >= 0 && numGrade <= 60){
+            grade = "F";
+        }
+        System.out.println("Grade: " + grade);
+    }
+}
