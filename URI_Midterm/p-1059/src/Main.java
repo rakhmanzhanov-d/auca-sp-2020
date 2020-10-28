99514e30ba63d7110d96f871a0af854cd00ec92f
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-10-28 13:50:30 +0600

    URI p-1059-Accepted

--- URI_Midterm/p-1059/src/Main.java
+++ URI_Midterm/p-1059/src/Main.java
@@ -4,9 +4,15 @@ public class Main {
     public static void main(String[] args) {
 //        Scanner inp = new Scanner(System.in);
         int i = 2;
-        while(i <= 100){
+//        while(i <= 100){
+//            System.out.println(i);
+//            i += 2;
+//        }
+
+        //Solving the problem using for
+        // For is used for definite numbers
+        for(i = 2; i <= 100; i+=2){
             System.out.println(i);
-            i += 2;
         }
     }
 }
