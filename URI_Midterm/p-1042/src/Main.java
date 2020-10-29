85ed910d8b4b2d1ba4a9eb25cf55ca6ad3c8fc88
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-10-29 16:44:41 +0600

    URI p-1042-Accepted

--- URI_Midterm/p-1042/src/Main.java
+++ URI_Midterm/p-1042/src/Main.java
@@ -3,33 +3,72 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int A = inp.nextInt();
-        int B = inp.nextInt();
-        int C = inp.nextInt();
+        int a = inp.nextInt();
+        int b = inp.nextInt();
+        int c = inp.nextInt();
 
-        int middle = 0;
-        int AB = (A + B + Math.abs(A - B)) / 2;
-        int ABC = (AB + C + Math.abs(AB - C)) / 2;
-        int ABmin = (A + B - Math.abs(A - B)) / 2;
-        int ABCmin = (ABmin + C - Math.abs(ABmin - C)) / 2;
+        int max = 0, mid = 0, min = 0;
 
-        if(A != ABC && A != ABCmin){
-            middle = A;
-        }else if(B != ABC && B != ABCmin){
-            middle = B;
-        }else if(C != ABC && C != ABCmin){
-            middle = C;
+        if(a < b && a < c){
+            min = a;
+            if(b > c){
+                max = b;
+                mid = c;
+            }else {
+                max = c;
+                mid = b;
+            }
+        }else if(b < a && b < c){
+            min = b;
+            if(a > c){
+                max = a;
+                mid = c;
+            }else {
+                max = c;
+                mid = a;
+            }
+        }else if(c < a && c < b){
+            min = c;
+            if(a > b){
+                max = a;
+                mid = b;
+            }else {
+                max = b;
+                mid = a;
+            }
+        }
+        if(a == b && b < c){
+            min = a;
+            mid = b;
+            max = c;
+        }else if(a == b && b > c){
+            min = c;
+            mid = b;
+            max = a;
+        }else if(b == c && c < a){
+            min = c;
+            mid = b;
+            max = a;
+        }else if(b == c && c > a){
+            min = a;
+            mid = b;
+            max = c;
+        }else if(b == a &&  b == c){
+            min = a;
+            mid = b;
+            max = c;
         }
 
-        System.out.println(ABCmin);
-        System.out.println(middle);
-        System.out.println(ABC);
+        System.out.println(min);
+        System.out.println(mid);
+        System.out.println(max);
 
-        System.out.println("");
+        System.out.println();
 
-        System.out.println(ABC);
-        System.out.println(middle);
-        System.out.println(ABCmin);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
 
     }
 }
