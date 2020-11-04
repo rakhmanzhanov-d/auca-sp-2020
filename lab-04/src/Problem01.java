36bce413e5200889ce94f6edbec3f5f90a1f7ebb
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-11-04 20:10:31 +0600

    Problem-01 Solved

--- lab-04/src/Problem01.java
+++ lab-04/src/Problem01.java
@@ -2,7 +2,14 @@ import java.util.Scanner;
 
 public class Problem01 {
     public static void main(String[] args) {
-        Scanner inp = new Scanner(System.in);
-
+        double d = 0.1;
+        double dd = d + d + d + d + d + d + d + d + d + d;
+        String res = "";
+        if(dd == 1.0){
+            res = "Equal";
+        }else {
+            res = "Not equal";
+        }
+        System.out.println(res);
     }
 }
