41ca34486c6b2b08dcea97de826e447aded53f5b
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-11-05 10:21:55 +0600

    URI p-1052-Aceepted

---
+++ URI_Midterm/p-1052/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int numMonth = inp.nextInt();;
+        String month = "";
+        if(numMonth > 0 && numMonth < 13){
+            switch(numMonth){
+                case 1: month = "January";
+                break;
+                case 2: month = "February";
+                    break;
+                case 3: month = "March";
+                    break;
+                case 4: month = "April";
+                    break;
+                case 5: month = "May";
+                    break;
+                case 6: month = "June";
+                    break;
+                case 7: month = "July";
+                    break;
+                case 8: month = "August";
+                    break;
+                case 9: month = "September";
+                    break;
+                case 10: month = "October";
+                    break;
+                case 11: month = "November";
+                    break;
+                case 12: month = "December";
+            }
+            System.out.println(month);
+        }
+    }
+}
