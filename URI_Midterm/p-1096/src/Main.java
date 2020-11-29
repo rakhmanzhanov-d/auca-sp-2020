96908492acd5a5d227797762a61e52d6a7d3f960
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-11-29 21:48:59 +0600

    URI p-1096-Accepted

---
+++ URI_Midterm/p-1096/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        for (int i = 1; i < 10; i += 2){
+            System.out.println("I="+i+ " J="+7);
+            System.out.println("I="+i+ " J="+6);
+            System.out.println("I="+i+ " J="+5);
+        }
+    }
+}
