6e86ead34fa2f90f53f3c6c50fdd451aaa0f09a6
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-12-14 17:41:51 +0600

    URI_Final p-1186-Accepted

---
+++ URI_Final/p-1186/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        float[][] arr = new float[12][12];
+        float res = 0;
+        char operation = inp.next().charAt(0);
+
+
+        for (int i = 0; i < 12; i++){
+            for (int t = 0; t < 12; t++){
+                arr[t][i] = inp.nextFloat();
+            }
+        }
+
+        int iteration = 11;
+        for(int i = 1; i < 12; i++){
+            for(int t = iteration; t > 0; t--){
+                res += arr[i][t];
+            }
+            iteration--;
+        }
+
+        if (operation == 'M') {
+            res = res / 66;
+        };
+
+        System.out.printf("%.1f\n" , res);
+    }
+}
