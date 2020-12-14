92ceb59e8078a59faa24e33df757b40b9f138384
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-12-14 15:35:48 +0600

    URI_Final p-1181-Accepted

---
+++ URI_Final/p-1181/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        float[][] arr = new float[12][12];
+        float res = 0;
+
+        int index = inp.nextInt();
+        char operation = inp.next().charAt(0);
+
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                arr[i][j] = inp.nextFloat();
+                if (i == index) {
+                    res += arr[i][j];
+                }
+            }
+        }
+
+        if (operation == 'M') {
+            res = res / 12;
+        }
+
+        System.out.printf("%.1f\n", res);
+    }
+}
