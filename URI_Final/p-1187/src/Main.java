6b2b3a302a8d0201067a75a367fef0f2e94119be
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-12-14 19:10:48 +0600

    URI_Final p-1187-Accepted

---
+++ URI_Final/p-1187/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        double[][] arr = new double[12][12];
+        double res = 0;
+        char O = inp.next().toUpperCase().charAt(0);
+
+
+        for (int i = 0; i < 12; i++){
+            for (int t = 0; t < 12; t++){
+                arr[i][t] = inp.nextDouble();
+            }
+        }
+
+        int start = 1;
+        int end = 10;
+        for(int i = 0; i < 5; i++){
+            for (int t = start; t <= end; t++){
+                res += arr[i][t];
+            }
+            start++;
+            end--;
+        }
+
+        if (O == 'M') {
+            res = res / 30;
+        };
+
+        System.out.printf("%.1f\n" , res);
+    }
+}
