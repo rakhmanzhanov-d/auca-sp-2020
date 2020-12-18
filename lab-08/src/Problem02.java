d94077c3dd328033c725a3210a843acbeb4066a9
Dastan Rakhmanzhanov <dastanrahmanzhanov0017@gmail.com>
2020-12-18 21:20:42 +0600

    Problem-02-Solved

---
+++ lab-08/src/Problem02.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        char[][] answers = {
+                {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+                {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+                {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+                {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+                {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+        };
+        // Key to the questions
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+
+        for (int i = 0; i < answers.length; i++){
+                int correctAnswer = 0;
+            for (int t = 0; t < answers[i].length; t++){
+                if(answers[i][t] == keys[t]){
+                    correctAnswer++;
+                }
+            }
+            System.out.println("Student " + i + "'s correct count is " + correctAnswer);
+        }
+
+    }
+}
